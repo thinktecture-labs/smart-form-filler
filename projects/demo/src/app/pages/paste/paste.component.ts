1208c3df15233c3f413f252ec35542c1bb49c74f
Christian Liebel <christian.liebel@thinktecture.com>
2024-03-15 09:57:54 -0700

    feat(demo): add form

--- projects/demo/src/app/pages/paste/paste.component.ts
+++ projects/demo/src/app/pages/paste/paste.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-paste',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    JsonPipe,
+  ],
   templateUrl: './paste.component.html',
   styleUrl: './paste.component.css'
 })
 export class PasteComponent {
+  private readonly fb = inject(NonNullableFormBuilder);
 
+  // TODO: Groups, arrays
+  protected readonly formGroup = this.fb.group({
+    firstName: [''],
+    lastName: [''],
+    phoneNumber: [''],
+    addressLine1: [''],
+    addressLine2: [''],
+    city: [''],
+    state: [''],
+    zip: [''],
+    country: [''],
+  })
 }
