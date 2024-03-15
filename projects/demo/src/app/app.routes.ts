5c0ad2e7d2b44bf4eb84826080c69732f1542c74
Christian Liebel <christian.liebel@thinktecture.com>
2024-03-15 09:25:16 -0700

    feat(demo): paste page

--- projects/demo/src/app/app.routes.ts
+++ projects/demo/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { PasteComponent } from './pages/paste/paste.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path: '',
+  redirectTo: 'paste',
+  pathMatch: 'full'
+}, {
+  path: 'paste',
+  component: PasteComponent
+}];
---
+++ projects/demo/src/app/pages/paste/paste.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paste',
+  standalone: true,
+  imports: [],
+  templateUrl: './paste.component.html',
+  styleUrl: './paste.component.css'
+})
+export class PasteComponent {
+
+}
