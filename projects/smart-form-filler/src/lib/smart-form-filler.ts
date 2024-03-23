270bb164936f64e4ed705344110f2c14ec3a1a98
Christian Liebel <christian.liebel@thinktecture.com>
2024-03-23 14:48:51 +0100

    feat(smart-form-filler): improve typing for `getFormFieldsFromFormGroup()`

--- projects/smart-form-filler/src/lib/smart-form-filler.ts
+++ projects/smart-form-filler/src/lib/smart-form-filler.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { AbstractControl, FormGroup } from '@angular/forms';
 import { ModelBackend } from './backends/model-backend';
 import { CompletedFormField } from './completed-form-field';
 import { FormField } from './form-field';
@@ -28,11 +28,19 @@ export class SmartFormFiller {
     return this.promptHandler.parseResponse(response);
   }
 
-  getFormFieldsFromFormGroup(formGroup: FormGroup, descriptions: { [key: string]: string } = {}): FormField[] {
-    return Object.keys(formGroup.controls).map(key => ({
+  /**
+   * Get from fields from an Angular form group. Optionally, you can add descriptions which the model may use as a hint.
+   * @param formGroup An Angular form group.
+   * @param descriptions Optional descriptions for fields
+   * @returns An array of form fields that can be used for the `getCompletions()` method.
+   */
+  getFormFieldsFromFormGroup<TControl extends {
+    [K in keyof TControl]: AbstractControl<any>;
+  } = any>(formGroup: FormGroup<TControl>, descriptions: Partial<{ [K in keyof TControl]: string }> = {}): FormField[] {
+    return Object.keys(formGroup.controls).map((key) => ({
       key: key,
       type: typeof formGroup.get(key)!.value,
-      description: descriptions[key],
+      description: descriptions[key as keyof TControl],
     }));
   }
 }
