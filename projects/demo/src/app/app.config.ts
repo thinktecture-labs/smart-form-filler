50e5dd0abda8a6c220dbef0741898a773d670f5c
Christian Liebel <christian.liebel@thinktecture.com>
2024-04-17 16:01:53 +0200

    feat(demo): use groq config

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -3,15 +3,19 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
-import { provideSmartFormFiller, withOpenAIToolsBackend } from '../../../smart-form-filler/src/public-api';
+import {
+  provideSmartFormFiller,
+  withOpenAIBackend,
+  withOpenAIToolsBackend,
+} from '../../../smart-form-filler/src/public-api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideSmartFormFiller(withOpenAIToolsBackend({
-      baseURL: 'http://localhost:4200/api/inference/',
-      //model: 'mixtral-8x7b-32768'
+    provideSmartFormFiller(withOpenAIBackend({
+      baseURL: `${location.origin}/api/inference/`,
+      model: 'mixtral-8x7b-32768'
     })),
   ]
 };
