ef5dd62cbff0a7a7a5eb438e1c3f93d66cafd8c2
Felix Schütz <felix.schuetz@thinktecture.com>
2024-04-18 14:49:38 +0200

    Add whisper endpoint to proxy config

--- proxy.conf.mjs
+++ proxy.conf.mjs
@@ -1,14 +1,24 @@
 import 'dotenv/config';
 
 export default {
-  '/api/inference': {
-    'target': 'https://api.groq.com/openai/v1', // https://api.openai.com/v1
-    'pathRewrite': {
-      '^/api/inference': ''
+  "/api/inference": {
+    target: "https://api.groq.com/openai/v1", // https://api.openai.com/v1
+    pathRewrite: {
+      "^/api/inference": "",
     },
-    'changeOrigin': true,
-    'bypass': function (req) {
-      req.headers['authorization'] = `Bearer ${process.env.OPENAI_API_KEY}`;
-    }
-  }
+    changeOrigin: true,
+    bypass: function (req) {
+      req.headers["authorization"] = `Bearer ${process.env.GROQ_API_KEY}`;
+    },
+  },
+  "/api/whisper": {
+    target: "https://api.openai.com/v1/audio/transcriptions",
+    pathRewrite: {
+      "^/api/whisper": "",
+    },
+    changeOrigin: true,
+    bypass: function (req) {
+      req.headers["authorization"] = `Bearer ${process.env.OPENAI_API_KEY}`;
+    },
+  },
 };
