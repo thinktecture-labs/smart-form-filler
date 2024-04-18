dbf158ff163be32bb74a664d73cbc404c3b15005
Felix Schütz <felix.schuetz@thinktecture.com>
2024-04-18 15:21:12 +0200

    Remove unused import

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -1,21 +1,21 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-import { routes } from './app.routes';
+import { provideRouter } from '@angular/router';
 import {
   provideSmartFormFiller,
   withOpenAIBackend,
-  withOpenAIToolsBackend,
 } from '../../../smart-form-filler/src/public-api';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideSmartFormFiller(withOpenAIBackend({
-      baseURL: `${location.origin}/api/inference/`,
-      model: 'mixtral-8x7b-32768'
-    })),
-  ]
+    provideSmartFormFiller(
+      withOpenAIBackend({
+        baseURL: `${location.origin}/api/inference/`,
+        model: 'mixtral-8x7b-32768',
+      }),
+    ),
+  ],
 };
