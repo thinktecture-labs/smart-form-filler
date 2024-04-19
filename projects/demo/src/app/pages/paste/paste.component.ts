f2cfcc690fcd0b75dd0590cf83874ec1fc00c2ec
Felix Schütz <felix.schuetz@thinktecture.com>
2024-04-19 14:08:15 +0200

    Add select to choose model

--- projects/demo/src/app/pages/paste/paste.component.ts
+++ projects/demo/src/app/pages/paste/paste.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, model, signal } from '@angular/core';
 import { NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
@@ -8,9 +8,10 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { firstValueFrom } from 'rxjs';
 import { SmartFormFiller } from '../../../../../smart-form-filler/src/public-api';
 import { AudioRecordingService } from '../../audio-recording/audio-recording.service';
-import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-paste',
@@ -24,6 +25,7 @@ import { firstValueFrom } from 'rxjs';
     MatRadioModule,
     MatCheckboxModule,
     MatDatepickerModule,
+    MatSelectModule,
   ],
   templateUrl: './paste.component.html',
   styleUrl: './paste.component.css',
@@ -37,6 +39,19 @@ export class PasteComponent {
   protected readonly inferencing = signal(false);
   protected readonly listening = signal(false);
   protected readonly transcribedText = signal('');
+  protected readonly models = [
+    {
+      label: 'Mixtral 7B (Groq)',
+      endpoint: 'groq/openai/v1',
+      model: 'mixtral-8x7b-32768',
+    },
+    {
+      label: 'Mistral Small',
+      endpoint: 'mistral/v1',
+      model: 'mistral-small-latest',
+    },
+  ];
+  protected readonly model = model(0);
   protected readonly formGroup = this.fb.group({
     date: [''],
     make: [''],
@@ -81,7 +96,7 @@ export class PasteComponent {
     this.inferencing.set(true);
 
     const userData = await navigator.clipboard.readText();
-    await this.inference(userData);
+    await this.fillForm(userData);
 
     this.inferencing.set(false);
   }
@@ -102,7 +117,7 @@ export class PasteComponent {
         this.audioRecordingService.transcribe(blob),
       );
       this.transcribedText.set(response.text);
-      this.inference(response.text);
+      this.fillForm(response.text);
     } catch (err) {
       console.error(err);
     } finally {
@@ -110,13 +125,20 @@ export class PasteComponent {
     }
   }
 
-  private async inference(userData: string): Promise<void> {
+  private async fillForm(userData: string): Promise<void> {
     try {
+      const model = this.models[this.model()];
       const completions = await this.formFiller.getCompletions(
         this.fields,
         userData,
+        {
+          baseURL: `${location.origin}/api/${model.endpoint}`,
+          model: model.model,
+        },
       );
+
       this.formGroup.reset();
+
       completions.forEach(({ key, value }) =>
         this.formGroup.get(key)?.setValue(value),
       );
