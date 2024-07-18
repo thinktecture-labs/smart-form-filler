bed26656bf2a3ac4210b62021f680755d43475d0
Yannick Baron <yb@thinktecture.com>
2024-07-18 18:14:55 +0200

    feat(smart-form-filler): add way to override prompt handler

--- projects/demo/src/app/app.config.ts
+++ projects/demo/src/app/app.config.ts
@@ -1,17 +1,23 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideRouter } from '@angular/router';
 
+import {
+  provideSmartFormFiller,
+  withOpenAIToolsBackend,
+} from '../../../smart-form-filler/src/public-api';
 import { routes } from './app.routes';
-import { provideSmartFormFiller, withOpenAIToolsBackend } from '../../../smart-form-filler/src/public-api';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideSmartFormFiller(withOpenAIToolsBackend({
-      baseURL: 'http://localhost:4200/api/inference/',
-      //model: 'mixtral-8x7b-32768'
-    })),
-  ]
+    provideSmartFormFiller(
+      withOpenAIToolsBackend({
+        baseURL: 'http://localhost:4200/api/inference/',
+        //model: 'mixtral-8x7b-32768'
+      }),
+      // withCustomPromptHandler(EnglishTextPromptHandler),
+    ),
+  ],
 };
--- projects/smart-form-filler/src/lib/providers.ts
+++ projects/smart-form-filler/src/lib/providers.ts
@@ -1,26 +1,28 @@
 // noinspection JSNonASCIINames,NonAsciiCharacters
 
-import { EnvironmentProviders, makeEnvironmentProviders, Provider } from '@angular/core';
-import { PromptAPIBackend } from './backends/prompt-api-backend';
+import { EnvironmentProviders, makeEnvironmentProviders, Provider, Type } from '@angular/core';
 import { ModelBackend } from './backends/model-backend';
 import { OpenAIBackend } from './backends/open-ai-backend';
 import { OPEN_AI_CONFIG, OpenAIConfig } from './backends/open-ai-config';
 import { OpenAIToolsBackend } from './backends/open-ai-tools-backend';
+import { PromptAPIBackend } from './backends/prompt-api-backend';
 import { WebLLMBackend } from './backends/web-llm-backend';
 import { WEB_LLM_CONFIG, WebLLMConfig } from './backends/web-llm-config';
-import { SmartFormFiller } from './smart-form-filler';
 import { JsonToolPromptHandler } from './prompt-handler/json-tool-prompt-handler';
 import { PromptHandler } from './prompt-handler/prompt-handler';
 import { TextPromptHandler } from './prompt-handler/text-prompt-handler';
+import { SmartFormFiller } from './smart-form-filler';
 
 interface SmartFormFillerFeature {
   ɵproviders: Provider[];
 }
 
-export function provideSmartFormFiller(feature: SmartFormFillerFeature): EnvironmentProviders {
+export function provideSmartFormFiller(
+  ...features: SmartFormFillerFeature[]
+): EnvironmentProviders {
   return makeEnvironmentProviders([
     SmartFormFiller,
-    feature.ɵproviders,
+    features.flatMap(feature => feature.ɵproviders),
   ]);
 }
 
@@ -60,5 +62,13 @@ export function withPromptAPIBackend(): SmartFormFillerFeature {
       { provide: ModelBackend, useClass: PromptAPIBackend },
       { provide: PromptHandler, useClass: TextPromptHandler },
     ],
+  };
+}
+
+export function withCustomPromptHandler(
+  handler: Type<PromptHandler<unknown>>,
+): SmartFormFillerFeature {
+  return {
+    ɵproviders: [{ provide: PromptHandler, useClass: handler }],
   };
 }
